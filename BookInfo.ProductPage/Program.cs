511ad819c302b4e0b4b2c6e8e24dd8cd9cd22ac8
Sevi Karaköse <sevikarakulak@gmail.com>
2020-07-06 16:45:06 +0300

    added Serilog with Elastic Common Schema

--- BookInfo.ProductPage/Program.cs
+++ BookInfo.ProductPage/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Elastic.CommonSchema.Serilog;
+using Microsoft.AspNetCore.Http;
+using Serilog;
 
 namespace BookInfo.ProductPage
 {
@@ -18,6 +21,14 @@ namespace BookInfo.ProductPage
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((ctx, config) =>
+                {
+                    config.ReadFrom.Configuration(ctx.Configuration);
+                    var formatterConfig = new EcsTextFormatterConfiguration();
+                    formatterConfig.MapHttpContext(ctx.Configuration.Get<HttpContextAccessor>());
+                    var formatter = new EcsTextFormatter(formatterConfig);
+                    config.WriteTo.Console(formatter);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>().UseUrls(Environment.GetEnvironmentVariable("BASE_URL") ?? "http://*:5110");
