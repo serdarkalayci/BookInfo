ef5a622a8622ad4ec141cadc1868658d2548a314
Sevi Karaköse <sevi.karakose@intertech.com.tr>
2020-10-27 10:25:52 +0300

    Successful logs added

--- BookInfo.Reviews/Controllers/ReviewsController.cs
+++ BookInfo.Reviews/Controllers/ReviewsController.cs
@@ -37,24 +37,52 @@ namespace BookInfo.Reviews.Controllers
         [Route("{bookId:int}")]
         public async Task<IActionResult> GetSingle(int bookId)
         {
+            _logger.LogInformation($"Fetching reviews for BookId:{bookId}");
             Dto.ReviewResult result = await GetRating(bookId);
-            var reviews = _reviewContext.Reviews.Where(x => x.BookId == bookId);
-            var reviewsDto = reviews.Map<Models.Review, Dto.Review>();
-            result.Reviews = reviewsDto.ToArray();
-            return Ok(result);
+
+            if (result == null)
+            {
+                _logger.LogError($"Can not retrieve ratings for BookId:{bookId}");
+                return NoContent();
+            }
+
+            try
+            {
+                var reviews = _reviewContext.Reviews.Where(x => x.BookId == bookId);
+                var reviewsDto = reviews.Map<Models.Review, Dto.Review>();
+                result.Reviews = reviewsDto.ToArray();
+                _logger.LogInformation($"Review fetched successfully for BookId:{bookId}");
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Can not retrieve reviews for BookId:{bookId}");
+                return NoContent();
+            }
         }
 
-        private async Task<Dto.ReviewResult> GetRating(int bookId) 
+        private async Task<Dto.ReviewResult> GetRating(int bookId)
         {
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Review Service");
-            string serviceURL = System.Environment.GetEnvironmentVariable("RATING_URL") ?? "http://localhost:5112";
-            serviceURL += "/ratings/" + bookId;
-            var streamTask = client.GetStreamAsync(serviceURL);
-            var result = await JsonSerializer.DeserializeAsync<Dto.ReviewResult>(await streamTask);
-            return result;        
+            _logger.LogInformation($"Fetching ratings for BookId:{bookId}");
+
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Review Service");
+                string serviceURL = System.Environment.GetEnvironmentVariable("RATING_URL") ?? "http://localhost:5112";
+                serviceURL += "/ratings/" + bookId;
+                var streamTask = client.GetStreamAsync(serviceURL);
+                var result = await JsonSerializer.DeserializeAsync<Dto.ReviewResult>(await streamTask);
+                _logger.LogInformation($"Ratings fetched successfully for BookId:{bookId}");
+                return result;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Ratings service does not return values for BookId:{bookId}");
+                return null;
+            }
         }
     }
 }
